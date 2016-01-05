d0c75989f8de6c34287e350358826eba0a2695a6
Kassidy Douglas <kcmdouglas@gmail.com>
2016-01-05 10:56:21 -0800

    Change Scabble code to convert char to string, make new test to recieve string.

--- src/main/java/Scrabble.java
+++ src/main/java/Scrabble.java
@@ -3,10 +3,27 @@ import java.util.HashMap;
 public class Scrabble {
   public static void main(String[] args) {}
 
-  public Integer scrabbleScore(String userWord) {
-    HashMap<String, Integer> scrabbleKey = new HashMap<String, Integer>();
-    scrabbleKey.put("a", 1);
+  public String scrabbleScore(String userWord) {
 
-    return scrabbleKey.get(userWord);
+    String[] scrabbleLetters = {"a"};
+    Integer[] scrabbleValues = {1};
+
+    char[] userCharacters = userWord.toCharArray();
+    char result = userCharacters[0];
+    String charString = Character.toString(result);
+
+    return charString;
+    // String[] userLetter = {};
+    // for (String characterLetter : Character.toString(userCharacters)){
+    //   userLetter.add(characterLetter);
+    // }
+    // String[] stringUserChars = String.valueOf(userCharacters);
+
+
+    // Integer total = 0;
+    //   for (Integer value : scrabbleKey.get(stringUserChars)) {
+    //     total = total + value;
+    //   }
+    // return total;
   }
 }
--- src/test/java/ScrabbleTest.java
+++ src/test/java/ScrabbleTest.java
@@ -3,17 +3,17 @@ import static org.junit.Assert.*;
 
 public class ScrabbleTest {
 
-  @Test
-  public void scrabbleScore_returnsAScrabbleScoreForALetter_1() {
-    Scrabble app = new Scrabble();
-    Integer score = 1;
-    assertEquals(score, app.scrabbleScore("a"));
-  }
+  // @Test
+  // public void scrabbleScore_returnsAScrabbleScoreForALetter_1() {
+  //   Scrabble app = new Scrabble();
+  //   Integer score = 1;
+  //   assertEquals(score, app.scrabbleScore("a"));
+  // }
 
   @Test
-  public void scrabbleScore_returnsAScrabbleScoreForTwoLetters_2() {
+  public void scrabbleScore_returnsAString_a() {
     Scrabble app = new Scrabble();
-    Integer score = 2;
-    assertEquals(score, app.scrabbleScore("a"));
+    String letter = "a";
+    assertEquals(letter, app.scrabbleScore("a"));
   }
 }
